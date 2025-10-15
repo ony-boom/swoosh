de7f9e10e754fa9bee82f3301edfe55a48e68c6a
Ony <ony@ony.world>
2025-10-15 18:38:31 +0300

    Bump swoosh version to 0.1.3

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
         packages.default = pkgs.buildGoModule {
           src = self;
           name = "swoosh";
-          version = "0.1.1";
+          version = "0.1.3";
           vendorHash = "sha256-bwHGOu5EGUU7Uw8Fe5Yswv8tN9uxFgjtVpx4wncmHAI=";
         };
 
